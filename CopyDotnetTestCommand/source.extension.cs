f716c3cfee7e125b9b63e00608b54a6c8de9aec8
Florian Gerlinghoff <gerlinghoff@live.de>
2020-11-05 15:00:21 +0100

    Added extension metadata to .vsixmanifest

--- CopyDotnetTestCommand/source.extension.cs
+++ CopyDotnetTestCommand/source.extension.cs
@@ -9,10 +9,10 @@ namespace CopyDotnetTestCommand
     {
         public const string Id = "CopyDotnetTestCommand.92c4e2aa-7c31-4a02-949b-29d58a1e1589";
         public const string Name = "CopyDotnetTestCommand";
-        public const string Description = @"With this extension, you can easily copy the .NET Core CLI command to execute one or several unit tests to the clipboard.";
+        public const string Description = @"Copy the 'dotnet test' command to execute one or several unit tests to the clipboard. Works with any method, class, or namespace in a .NET Core test project.";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "0.1";
         public const string Author = "Florian Gerlinghoff";
-        public const string Tags = "";
+        public const string Tags = "test, unit test, test explorer, cli";
     }
 }
