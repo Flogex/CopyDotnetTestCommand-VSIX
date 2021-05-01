c1c5adc5c83c4da731701c6916a386b7c515a366
Florian Gerlinghoff <gerlinghoff@live.de>
2021-05-01 16:07:12 +0300

    Added spaces to extension name

--- CopyDotnetTestCommand/source.extension.cs
+++ CopyDotnetTestCommand/source.extension.cs
@@ -8,7 +8,7 @@ namespace CopyDotnetTestCommand
     internal sealed partial class Vsix
     {
         public const string Id = "CopyDotnetTestCommand.92c4e2aa-7c31-4a02-949b-29d58a1e1589";
-        public const string Name = "CopyDotnetTestCommand";
+        public const string Name = "Copy Dotnet Test Command";
         public const string Description = @"Copy the 'dotnet test' command to execute one or several unit tests to the clipboard. Works with any method, class, or namespace in a .NET Core test project.";
         public const string Language = "en-US";
         public const string Version = "0.1";
